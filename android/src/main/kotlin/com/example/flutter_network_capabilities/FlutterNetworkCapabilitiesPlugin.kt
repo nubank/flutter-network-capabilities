827d8b26a0980fdb7f3bd454371e3d178de4f28b
Thiago Zagui Giacomini <thiago.giacomini@nubank.com.br>
2024-04-03 11:05:56 -0300

    Add all possible network information to Channel Result

--- android/src/main/kotlin/com/example/flutter_network_capabilities/FlutterNetworkCapabilitiesPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_network_capabilities/FlutterNetworkCapabilitiesPlugin.kt
@@ -4,26 +4,51 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import android.telephony.PhoneStateListener
-import android.telephony.SignalStrength
-import android.telephony.TelephonyCallback
 import android.telephony.TelephonyManager
-import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import java.util.concurrent.Executor
-
 
 /** FlutterNetworkCapabilitiesPlugin */
 class FlutterNetworkCapabilitiesPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var channel: MethodChannel
     private lateinit var connectivityManager: ConnectivityManager
-    private lateinit var telephonyManager: TelephonyManager
-    private var mainExecutor: Executor? = null
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    private val networkMobileSubtypes: Map<Int, String> by lazy {
+        mutableMapOf(
+            TelephonyManager.NETWORK_TYPE_GPRS to "network_type_gprs",
+            TelephonyManager.NETWORK_TYPE_EDGE to "network_type_edge",
+            TelephonyManager.NETWORK_TYPE_UMTS to "network_type_umts",
+            TelephonyManager.NETWORK_TYPE_CDMA to "network_type_cdma",
+            TelephonyManager.NETWORK_TYPE_EVDO_0 to "network_type_evdo_0",
+            TelephonyManager.NETWORK_TYPE_EVDO_A to "network_type_evdo_a",
+            TelephonyManager.NETWORK_TYPE_1xRTT to "network_type_1xrtt",
+            TelephonyManager.NETWORK_TYPE_HSDPA to "network_type_hsdpa",
+            TelephonyManager.NETWORK_TYPE_HSUPA to "network_type_hsupa",
+            TelephonyManager.NETWORK_TYPE_HSPA to "network_type_hspa",
+            TelephonyManager.NETWORK_TYPE_IDEN to "network_type_iden",
+            TelephonyManager.NETWORK_TYPE_EVDO_B to "network_type_evdo_b",
+            TelephonyManager.NETWORK_TYPE_LTE to "network_type_lte",
+            TelephonyManager.NETWORK_TYPE_EHRPD to "network_type_ehrpd",
+            TelephonyManager.NETWORK_TYPE_HSPAP to "network_type_hspap",
+        ).apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+                put(TelephonyManager.NETWORK_TYPE_GSM, "network_type_gsm")
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+                put(TelephonyManager.NETWORK_TYPE_TD_SCDMA, "network_type_td_scdma")
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+                put(TelephonyManager.NETWORK_TYPE_IWLAN, "network_type_iwlan")
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(TelephonyManager.NETWORK_TYPE_NR, "network_type_nr")
+            }
+        }
+    }
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel =
             MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_network_capabilities")
         channel.setMethodCallHandler(this)
@@ -31,93 +56,54 @@ class FlutterNetworkCapabilitiesPlugin : FlutterPlugin, MethodCallHandler {
         val context = flutterPluginBinding.applicationContext
         connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            mainExecutor = context.mainExecutor
-        }
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && mainExecutor != null) {
-            telephonyManager.registerTelephonyCallback(
-                mainExecutor!!,
-                object : TelephonyCallback(), TelephonyCallback.SignalStrengthsListener {
-                    override fun onSignalStrengthsChanged(signalStrength: SignalStrength) {
-                        println("[HUE] Signal Strength ${signalStrength.gsmSignalStrength}")
-                        println("[HUE] Signal Strength ${signalStrength.level}")
-                        println("[HUE] Signal Strength ${signalStrength.cellSignalStrengths}")
-                        println("[HUE] Signal Strength ${signalStrength.describeContents()}")
-                    }
-                })
-        }
-
-        telephonyManager.listen(object : PhoneStateListener() {
-            override fun onSignalStrengthsChanged(signalStrength: SignalStrength) {
-                super.onSignalStrengthsChanged(signalStrength)
-                println("[HUE] Signal Strength ${signalStrength.gsmSignalStrength}")
-                println("[HUE] Signal Strength ${signalStrength.describeContents()}")
-            }
-        }, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                val signalStrength = networkCapabilities?.signalStrength
-                println("[HUE-new]: $signalStrength")
-            }
-
-//            when {
-//                networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true -> {/* WIFI */}
-//                networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true -> {
-//                    // Cellular data, we can check the generation of the network
-//                    when {
-//                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-//                                !networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED) &&
-//                                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) -> {/* 2G */}
-//                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_IA) -> {/* 3G */}
-//                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_LTE) -> {/* 4G */}
-//                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NR)
-//                                || networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NR_MMWAVE) -> {/* 5G*/ }
-//                    }
-//                }
-
-            if (networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    println("[HUE-new] ${networkCapabilities.capabilities}")
-                }
-                println("[HUE-new] has2g: ${networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)}")
-                println("[HUE-new] has3g: ${networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_IA)}")
-            }
-        }
-
-        val networkInfo = connectivityManager.activeNetworkInfo
-        if (networkInfo != null && networkInfo.isConnected) {
-            println(networkInfo.detailedState.name)
-            when (networkInfo.type) {
-                ConnectivityManager.TYPE_MOBILE -> {
-                    val subtype: Int = networkInfo.subtype
-                    println(subtype)
-                    when (subtype) {
-                        TelephonyManager.NETWORK_TYPE_LTE -> println("[HUE-old] - 4g")
-                        TelephonyManager.NETWORK_TYPE_HSDPA, TelephonyManager.NETWORK_TYPE_HSPA, TelephonyManager.NETWORK_TYPE_HSUPA -> println(
-                            "[HUE-old] - 3g"
-                        )
-
-                        else -> println("[HUE-old] - 2g or unknown")
-                    }
-                }
-
-                ConnectivityManager.TYPE_WIFI -> println("[HUE] - wifi")
-            }
-        }
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method != "getNetworkInfo") {
             result.notImplemented()
         }
+
+        val networkInfoResult = mutableMapOf<String, Any>()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                networkCapabilities?.let {
+                    networkInfoResult["signal_strength"] = it.signalStrength
+                    networkInfoResult["link_downstream_bandwidth_kbps"] =
+                        it.linkDownstreamBandwidthKbps
+                    networkInfoResult["link_upstream_bandwidth_kbps"] = it.linkUpstreamBandwidthKbps
+                }
+            }
+
+            when {
+                networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true ->
+                    networkInfoResult["transport_type"] = "wifi"
+
+                networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true ->
+                    networkInfoResult["transport_type"] = "cellular"
+            }
+        }
+
+        // taking advantage of older Android API's that has more information
+        val networkInfo = connectivityManager.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnected) {
+            when (networkInfo.type) {
+                ConnectivityManager.TYPE_MOBILE -> {
+                    networkInfoResult["transport_type"] = "celullar"
+                    networkInfoResult["transport_subtype"] =
+                        networkMobileSubtypes[networkInfo.subtype] ?: "unknown"
+                }
+
+                ConnectivityManager.TYPE_WIFI -> networkInfoResult["transport_subtype"] = "wifi"
+            }
+        }
+
+        result.success(networkInfoResult)
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 }
