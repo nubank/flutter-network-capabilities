f2edd86c6dc36cd22c125427c1b918530ec054b7
Philippe Ehlert <philippe.ehlert@nubank.com.br>
2024-04-12 14:53:24 -0300

    Standardize result keys for iOS usage in the future
    Co-authored-by: Thiago Zagui Giacomini <thiago.giacomini@nubank.com.br>

--- android/src/main/kotlin/com/example/flutter_network_capabilities/FlutterNetworkCapabilitiesPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_network_capabilities/FlutterNetworkCapabilitiesPlugin.kt
@@ -70,19 +70,19 @@ class FlutterNetworkCapabilitiesPlugin : FlutterPlugin, MethodCallHandler {
                 connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 networkCapabilities?.let {
-                    networkInfoResult["network_capabilities_signal_strength"] = it.signalStrength
-                    networkInfoResult["network_capabilities_link_downstream_bandwidth_kbps"] =
+                    networkInfoResult["signal_strength"] = it.signalStrength
+                    networkInfoResult["link_downstream_bandwidth_kbps"] =
                         it.linkDownstreamBandwidthKbps
-                    networkInfoResult["network_capabilities_link_upstream_bandwidth_kbps"] = it.linkUpstreamBandwidthKbps
+                    networkInfoResult["link_upstream_bandwidth_kbps"] = it.linkUpstreamBandwidthKbps
                 }
             }
 
             when {
                 networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true ->
-                    networkInfoResult["network_capabilities_transport_type"] = "wifi"
+                    networkInfoResult["transport_type"] = "wifi"
 
                 networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true ->
-                    networkInfoResult["network_capabilities_transport_type"] = "cellular"
+                    networkInfoResult["transport_type"] = "cellular"
             }
         }
 
@@ -91,12 +91,12 @@ class FlutterNetworkCapabilitiesPlugin : FlutterPlugin, MethodCallHandler {
         if (networkInfo != null && networkInfo.isConnected) {
             when (networkInfo.type) {
                 ConnectivityManager.TYPE_MOBILE -> {
-                    networkInfoResult["network_info_transport_type"] = "celullar"
-                    networkInfoResult["network_info_transport_type_subtype"] =
+                    networkInfoResult["legacy_transport_type"] = "celullar"
+                    networkInfoResult["legacy_transport_subtype"] =
                         networkMobileSubtypes[networkInfo.subtype] ?: "unknown"
                 }
 
-                ConnectivityManager.TYPE_WIFI -> networkInfoResult["network_info_transport_type"] = "wifi"
+                ConnectivityManager.TYPE_WIFI -> networkInfoResult["legacy_transport_type"] = "wifi"
             }
         }
 
